dcd2d595e4c04e7dc690d7d872e67ac39a9e8404
Kamil Cinner <kamil.cinner@gmail.com>
2020-02-12 02:32:09 +0100

    Fixed active shopping lists counting

--- task_manager/views.py
+++ task_manager/views.py
@@ -14,7 +14,21 @@ from task_manager.models import Task, ShoppingList, ShoppingListItem
 
 
 def index_view(request):
-    return render(request, 'task_manager/index.html', context={})
+    if request.user.is_authenticated:
+        num_tasks = Task.objects.filter(owner__username__exact=request.user.username).count()
+        num_active_slist = 0
+        for slist in ShoppingList.objects.filter(owner__username__exact=request.user.username):
+            for item in slist.shoppinglistitem_set.all():
+                if not item.status:
+                    num_active_slist += 1
+                    break
+        context = {
+            'num_tasks': num_tasks,
+            'num_active_slist': num_active_slist
+        }
+    else:
+        context = {}
+    return render(request, 'task_manager/index.html', context=context)
 
 
 def inactive_yet_view(request):
